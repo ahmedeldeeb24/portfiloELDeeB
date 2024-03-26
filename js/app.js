1ccba5ab3c01a283b356c7cc0051de4658748d37
ahmedeldeeb24 <142840946+ahmedeldeeb24@users.noreply.github.com>
2024-03-26 17:23:17 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var typed = new Typed(".text", {
+  strings: ["Frontend", "Web Developer", "YouTuber"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+let section = document.querySelector(".three");
+let spans = document.querySelectorAll(".progress span");
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop - 1000) {
+    spans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+};
